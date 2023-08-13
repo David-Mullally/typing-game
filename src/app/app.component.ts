afc98c4f48b715ea5340ca4488c2ccc6d74db72f
Dave Mullally <mullallydavid7@gmail.com>
2023-08-13 20:41:28 +0200

    finish basci app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,22 @@ import { lorem } from 'faker';
 })
 export class AppComponent implements OnInit {
   randomSentence = "";
+  userInput = "";
+  successMsgHidden = true;
+  comparisonArray;
   constructor() { 
     this.randomSentence = lorem.sentence();
+    this.comparisonArray =[...this.randomSentence.split("")]
   }
   
-  ngOnInit(){}
+  ngOnInit() { }
+  
+  handleUserInput(event: any) {
+    this.userInput = event.target.value;
+    console.log(this.comparisonArray);
+  }
+
+  compareLetters(letter: string, userInputLetter: string) {
+    return !userInputLetter ? "pending" : letter === userInputLetter ? "match" : "noMatch"
+  }
 }
